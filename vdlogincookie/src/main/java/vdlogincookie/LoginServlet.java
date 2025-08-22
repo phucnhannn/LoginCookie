eaa79a65582d85dea00762f46a85cf678d7d1d65
Bùi Phúc Nhân <buiphucnhanbt@gmail.com>
2025-08-22 10:50:24 +0700

    Add files via upload

---
+++ vdlogincookie/src/main/java/vdlogincookie/LoginServlet.java
@@ -0,0 +1,36 @@
+package vdlogincookie;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns= {"/login"})
+public class LoginServlet extends HttpServlet{
+	 @Override
+	 protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, 
+	IOException {
+	 resp.setContentType("text/html");
+
+	 String user = req.getParameter("username");
+	 String pass = req.getParameter("password");
+	 if(user.equals("trung") && pass.equals("123"))
+	 {
+	 Cookie cookie = new Cookie("username", user); 
+	 
+	 cookie.setMaxAge(30);
+
+	 resp.addCookie(cookie);
+
+	 resp.sendRedirect(req.getContextPath() + "/hello");
+	 }else {
+
+	 resp.sendRedirect(req.getContextPath() + "/login");
+	 }
+	}
+
+}
